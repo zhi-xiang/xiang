0b7e36feb89b05e4a70720b4214c1ee7006cfe12
zhixiang <1430758469@qq.com>
2019-08-10 15:02:19 +0800

    第二次上传

--- app.js
+++ app.js
@@ -6,11 +6,31 @@ app.use('/uploads', express.static('uploads'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+// const db = require('./db.js')
+// app.get('/getHeroes', (req, res) => {
+//     let page = 1;
+//     let pageNum = 5;
+//     let sql = 'select * from heroes limit ' + (page - 1) * pageNum + ',' + pageNum;
+//     db(sql, null, (err, result) => {
+//         if (err) throw err;
+//         res.send(result)
+//     });
+// });
+
 const db = require('./db.js');
 app.get('/getHeroes', (req, res) => {
-    db('select * from heroes', null, (err, result) => {
+    let page = req.query.page || 1;
+    let pageNum = 5;
+    let sql = 'select * from heroes limit ' + (page - 1) * pageNum + ',' + pageNum;
+    sql += ';select count(*) c from heroes';
+    db(sql, null, (err, result) => {
         if (err) throw err;
-        res.send(result);
+        // res.send(result);
+        res.send({
+            data: result[0],
+            pageTotal: Math.ceil(result[1][0].c / pageNum)
+        });
     });
 });
 
@@ -26,7 +46,7 @@ app.post('/addHero', upload.single('heroIcon'), (req, res) => {
         file: req.file.path,
         skill: req.body.skillName
     };
-    db(sql, values, (err, result) => {  
+    db(sql, values, (err, result) => {
         if (err) {
             res.send({ code: 201, message: '添加失败' })
         } else {
@@ -35,4 +55,49 @@ app.post('/addHero', upload.single('heroIcon'), (req, res) => {
     })
 })
 
+app.get('/getHeroById', (req, res) => {
+    let id = req.query.id;
+    if (!id || isNaN(id)) {
+        res.send('参数错误')
+        return;
+    }
+    db('select * from heroes where id=?', id, (err, result) => {
+        if (err) throw err;
+        res.send(result[0]);
+    })
+})
 
+app.post('/updateHero', upload.single('heroIcon'), (req, res) => {
+    let sql = 'update heroes set ? where id=?';
+    let values = {
+        name: req.body.heroName,
+        nickname: req.body.heroNickName,
+        skill: req.body.skillName
+    };
+    if (req.file !== undefined) {
+        values.file = req.file.path;
+    };
+    db(sql, [values, req.body.id], (err, result) => {
+        if (err) {
+            res.send({ code: 201, message: '更新失败' })
+        } else {
+            res.send({ code: 200, message: '更新成功' })
+        }
+    })
+})
+
+
+app.get('/deleteHero', (req, res) => {
+    let id = req.query.id;
+    if (!id || isNaN(id)) {
+        res.send('参数错误');
+        return;
+    }
+    db('delete from heroes where id=?', id, (err, result) => {
+        if (err) {
+            res.send({ code: 201, message: '删除失败' });
+        } else {
+            res.send({ code: 200, message: '删除成功' });
+        }
+    })
+})
